cd18e8a2b5d817bb4b735b6b2ebbfda6e90bf7b5
sm-alamin <sma.fkml@gmail.com>
2023-05-02 10:03:58 +0600

    the design of chefs section is done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import BannerRight from "./BannerRight";
 import BannerLeft from "./BannerLeft";
+import Chefs from "./Chefs";
 
 const Home = () => {
   return (
-    <div className="flex gap-5 justify-center items-center w-11/12 mx-auto pt-5">
+    <div>
+      <div className="flex gap-5 justify-center items-center w-11/12 mx-auto pt-5">
       <BannerLeft />
       <BannerRight />
+    </div>
+    <section>
+      <Chefs/>
+    </section>
     </div>
   );
 };
